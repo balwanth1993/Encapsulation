9f0f3e78d6a4e36024bf6034d8be856904eed1cc
balwanth1993 <balwanth.r93@gmail.com>
2016-12-03 18:10:56 -0600

    Add project files.

---
+++ 2Encapsulation/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    public static void Main()
+    {
+        Encapsulation encp = new Encapsulation();
+        encp.ID = -100;
+       
+        Console.WriteLine("Id =" + encp.ID );
+
+    }
+}
+public class Encapsulation
+{
+    private  int id;
+    private string Name;
+    private int PassMarks;
+    public int ID
+    {
+        set
+        {
+            if (value <= 0)
+            {
+                throw new Exception("Id cannot be negative");
+            }
+            this.id = value;
+
+        }
+        get
+        {
+            return this.id;
+        }
+        
+        
+
+
+        
+    }
+    
+
+}
